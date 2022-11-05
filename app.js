fb8b862ec3f1c5828b7a90117d8eefe19af302e1
Abdullahmamji <117278033+Abdullahmamji@users.noreply.github.com>
2022-11-04 18:44:40 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,94 @@
+// alert ('welcome to javascript')
+
+
+// var abc ="welcome to javascript"
+// alert (abc)
+
+// var abc ="this is a javascript website"
+// alert (abc)
+
+
+// var a = 20;
+// console.log (a);
+
+
+// var a = 20;
+// var b = 50;
+// var c = (a + b) -a;
+// console.log (a);
+
+
+// var a = 10;
+// a++;
+// a--;
+// ++a;
+// --a; 
+
+// console.log (a);
+
+
+// var EnglishMarks = +prompt("Enter English Marks");
+// var UrduMarks = +prompt("Enter Urdu Marks");
+// var IslamiatMarks = +prompt("Enter Islamiat Marks");
+// var MathsMarks = +prompt("Enter Maths Marks");
+// var PhysicsMarks = +prompt("Enter Physics Marks")
+// var TotalMarks = EnglishMarks +UrduMarks +IslamiatMarks +MathsMarks +PhysicsMarks
+
+// console.log (EnglishMarks);
+// console.log (UrduMarks);
+// console.log (IslamiatMarks);
+// console.log (MathsMarks);
+// console.log (PhysicsMarks);
+// console.log (TotalMarks);
+
+
+// var percent = 100;
+
+// if (percent >70){
+//     console.log("Grade A")
+// }else if (percent >60){
+//     console.log("Grade B")
+// }else (percent > 50)
+// {
+//     console.log("Grade Fail")
+// }
+
+
+
+// var officer = false;
+// var assistant = true;
+
+
+// if (officer == false){
+//     console.log("assignment submitted")
+// }else if (assistant == true){
+//     console.log("assignment submitted to assistant")
+// }else{
+//     console.log("Wapis jao")
+// }
+
+
+
+// var age = 18;
+// var studentcard = true;
+
+// if (age == 18){
+//     console.log("Allow")
+// } else if (studentcard == true){
+//     console.log("Allow on Student Card")
+// }else{
+//     console.log("Not Allow")
+// }
+
+
+//  var age = 18;
+//  var studentcard = true;
+
+
+//  if (age = 18 && studentcard == true){
+//     console.log ("Allow")
+//  }else{
+//     console.log ("Not Allow")
+//  }
+
+
